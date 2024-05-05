647b7104e2b33599c02094e4093cdc86c98176af
Dash-02 <dasha.semivolos@gmail.com>
2024-05-05 03:22:12 +0300

    block-5

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -3,10 +3,12 @@ import React, {useState} from 'react';
 import style from './Filters.module.scss';
 import icoFilter from '../../assets/icons/filter.svg';
 import icoPoly from '../../assets/icons/polygon.svg';
-import icoReload from '../../assets/icons/reload_.svg';
+import icoReload from '../../assets/icons/reload.svg';
 import icoHeart from '../../assets/icons/favorite_heart.svg';
 import icoLock from '../../assets/icons/lock_open.svg';
 import icoVerify from '../../assets/icons/check_verify.svg';
+import icoHeartStroke from '../../assets/icons/favorite.svg';
+import icoSave from '../../assets/icons/save99.svg';
 
 function Filter() {
 
@@ -151,40 +153,40 @@ function Filter() {
                 {/*=========== Block 4 ===========*/}
                 <div className={style.filter_block}>
                     <div className={style.wrapper_radio_btn}>
-                        <input type="radio" id="btn_radio" />
-                        <label for="btn_radio" className={style.label_radio}>
+                        <input type="radio" id="like" />
+                        <label for="like" className={style.label_radio}>
                             В избранном
                             <img src={icoHeart} alt="" />
                         </label>
                     </div>
 
                     <div className={style.wrapper_radio_btn}>
-                        <input type="radio" id="btn_radio" />
-                        <label for="btn_radio" className={style.label_radio}>
+                        <input type="radio" id="online" />
+                        <label for="online" className={style.label_radio}>
                             Администратор онлайн
                             <div className={style.round}></div>
                         </label>
                     </div>
 
                     <div className={style.wrapper_radio_btn}>
-                        <input type="radio" id="btn_radio" />
-                        <label for="btn_radio" className={style.label_radio}>
+                        <input type="radio" id="open" />
+                        <label for="open" className={style.label_radio}>
                             Открытый канал
                             <img src={icoLock} alt="" />
                         </label>
                     </div>
 
                     <div className={style.wrapper_radio_btn}>
-                        <input type="radio" id="btn_radio" />
-                        <label for="btn_radio" className={style.label_radio}>
+                        <input type="radio" id="verify" />
+                        <label for="verify" className={style.label_radio}>
                             Верифицированный канал
                             <img src={icoVerify} alt="" />
                         </label>
                     </div>
 
                     <div className={style.wrapper_radio_btn}>
-                        <input type="radio" id="btn_radio" />
-                        <label for="btn_radio" className={style.label_radio}>
+                        <input type="radio" id="check" />
+                        <label for="check" className={style.label_radio}>
                             Проверенный канал
                         </label>
                     </div>
@@ -192,6 +194,17 @@ function Filter() {
                 </div>
 
                 {/*=========== Block 5 ===========*/}
+                <div className={style.wrapper_search}>
+                    <button className={style.btn_search}>
+                        <img src={icoHeartStroke} alt="" />
+                        Сохранить поиск
+                    </button>
+
+                    <button className={style.btn_search}>
+                        <img src={icoSave} alt="" />
+                        Восстановить поиск
+                    </button>
+                </div>
 
             </div>
             <img className={style.filter_ico} src={icoFilter} alt="filter" />
