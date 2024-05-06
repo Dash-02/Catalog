cbfffb9592568a37668890d31fcd77997ce3b6e7
Dash-02 <dasha.semivolos@gmail.com>
2024-05-06 19:49:03 +0300

    inputs-block3-block4

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -13,13 +13,17 @@ import {data} from '../FakeData/FakeData.js';
 import DropDown from '../DropDownFilt/DropDownFilt.jsx';
 
 function Filter() {
-
+    // ===== default_price ===== //
     const [priceRange, setPriceRange] = useState({ min: null, max: null });
     const minCost = 0;
     const maxCost = 100000;
 
     useEffect(() => {
         updatePriceRange();
+        updateCpmRange();
+        updateSubscribeRange();
+        updateViewsRange();
+        updateErrRange();
     }, []);
 
     const updatePriceRange = () => {
@@ -41,6 +45,98 @@ function Filter() {
     
     const handleReload = () => {
         updatePriceRange();
+    };
+
+    // ===== CPM ====== //
+    const [cpmRange, setCpmRange] = useState({ min: null, max: null });
+    const updateCpmRange = () => {
+        const cpm = data.map(item => item.CPM);
+        const min = Math.min(...cpm);
+        const max = Math.max(...cpm);
+        setCpmRange({ min, max });
+    };
+
+    const handleCpmChange = (event) => {
+        const cpm = event.target.value;
+        setCpmRange(prevState => ({ ...prevState, min: cpm }));
+    };
+
+    const handleMaxCpmChange = (event) => {
+        const cpm = event.target.value;
+        setCpmRange(prevState => ({ ...prevState, max: cpm }));
+    };
+    
+    const handleReloadCpm = () => {
+        updateCpmRange();
+    };
+
+    // ===== count_subscribers ===== //
+    const [subscribeRange, setSubscribeRange] = useState({ min: null, max: null });
+    const updateSubscribeRange = () => {
+        const subscribe = data.map(item => item.count_subscribers);
+        const min = Math.min(...subscribe);
+        const max = Math.max(...subscribe);
+        setSubscribeRange({ min, max });
+    };
+
+    const handleSubscribeChange = (event) => {
+        const subscribe = event.target.value;
+        setSubscribeRange(prevState => ({ ...prevState, min: subscribe }));
+    };
+
+    const handleMaxSubscribeChange = (event) => {
+        const subscribe = event.target.value;
+        setSubscribeRange(prevState => ({ ...prevState, max: subscribe }));
+    };
+
+    const handleReloadSubscribe = () => {
+        updateSubscribeRange();
+    };
+
+    // ===== count_views ===== //
+    const [viewsRange, setViewsRange] = useState({ min: null, max: null });
+    const updateViewsRange = () => {
+        const views = data.map(item => item.count_views);
+        const min = Math.min(...views);
+        const max = Math.max(...views);
+        setViewsRange({ min, max });
+    };
+
+    const handleViewsChange = (event) => {
+        const views = event.target.value;
+        setViewsRange(prevState => ({ ...prevState, min: views }));
+    };
+
+    const handleMaxViewsChange = (event) => {
+        const views = event.target.value;
+        setViewsRange(prevState => ({ ...prevState, max: views }));
+    };
+
+    const handleReloadViews = () => {
+        updateViewsRange();
+    };
+    
+    // ===== ERR ===== //
+    const [errRange, setErrRange] = useState({ min: null, max: null });
+    const updateErrRange = () => {
+        const err = data.map(item => item.ERR);
+        const min = Math.min(...err);
+        const max = Math.max(...err);
+        setErrRange({ min, max });
+    };
+
+    const handleErrChange = (event) => {
+        const err = event.target.value;
+        setErrRange(prevState => ({ ...prevState, min: err }));
+    };
+
+    const handleMaxErrChange = (event) => {
+        const err = event.target.value;
+        setErrRange(prevState => ({ ...prevState, max: err }));
+    };
+
+    const handleReloadErr = () => {
+        updateErrRange();
     };
 
     const categoryList = [
@@ -118,14 +214,26 @@ function Filter() {
                         <div className={style.filter_content}>
                             <label className={style.label_wrapper}>
                                 <span>от</span>
-                                <input type="number" min={minCost} max={maxCost}/>
+                                <input 
+                                    type="number" 
+                                    min={minCost} 
+                                    max={maxCost}
+                                    value={cpmRange.min || ''}
+                                    onChange={handleCpmChange}
+                                />
                             </label>
                             <label className={style.label_wrapper}>
                                 <span>до</span>
-                                <input type="number" min={minCost} max={maxCost}/>
+                                <input 
+                                    type="number" 
+                                    min={minCost} 
+                                    max={maxCost}
+                                    value={cpmRange.max || ''}
+                                    onChange={handleMaxCpmChange}
+                                />
                             </label>
                     
-                            <button className={style.reload_btn} onClick={handleReload}>
+                            <button className={style.reload_btn} onClick={handleReloadCpm}>
                                 <img src={icoReload} alt="" />
                             </button>
                         </div>
@@ -141,14 +249,26 @@ function Filter() {
                         <div className={style.filter_content}>
                             <label className={style.label_wrapper}>
                                 <span>от</span>
-                                <input type="number" min={minCost} max={maxCost}/>
+                                <input 
+                                    type="number" 
+                                    min={minCost} 
+                                    max={maxCost}
+                                    value={subscribeRange.min || ''}
+                                    onChange={handleSubscribeChange}
+                                />
                             </label>
                             <label className={style.label_wrapper}>
                                 <span>до</span>
-                                <input type="number" min={minCost} max={maxCost}/>
+                                <input 
+                                    type="number" 
+                                    min={minCost} 
+                                    max={maxCost}
+                                    value={subscribeRange.max || ''}
+                                    onChange={handleMaxSubscribeChange}
+                                />
                             </label>
                     
-                            <button className={style.reload_btn}>
+                            <button className={style.reload_btn} onClick={handleReloadSubscribe}>
                                 <img src={icoReload} alt="" />
                             </button>
                         </div>
@@ -161,14 +281,26 @@ function Filter() {
                         <div className={style.filter_content}>
                             <label className={style.label_wrapper}>
                                 <span>от</span>
-                                <input type="number" min={minCost} max={maxCost}/>
+                                <input 
+                                    type="number" 
+                                    min={minCost} 
+                                    max={maxCost}
+                                    value={viewsRange.min || ''}
+                                    onChange={handleViewsChange}
+                                />
                             </label>
                             <label className={style.label_wrapper}>
                                 <span>до</span>
-                                <input type="number" min={minCost} max={maxCost}/>
+                                <input 
+                                    type="number" 
+                                    min={minCost} 
+                                    max={maxCost}
+                                    value={viewsRange.max || ''}
+                                    onChange={handleMaxViewsChange}
+                                />
                             </label>
                     
-                            <button className={style.reload_btn}>
+                            <button className={style.reload_btn} onClick={handleReloadViews}>
                                 <img src={icoReload} alt="" />
                             </button>
                         </div>
@@ -181,14 +313,26 @@ function Filter() {
                         <div className={style.filter_content}>
                             <label className={style.label_wrapper}>
                                 <span>от</span>
-                                <input type="number" min={minCost} max={maxCost}/>
+                                <input 
+                                    type="number" 
+                                    min={minCost} 
+                                    max={maxCost}
+                                    value={errRange.min || ''}
+                                    onChange={handleErrChange}
+                                />
                             </label>
                             <label className={style.label_wrapper}>
                                 <span>до</span>
-                                <input type="number" min={minCost} max={maxCost}/>
+                                <input 
+                                    type="number" 
+                                    min={minCost} 
+                                    max={maxCost}
+                                    value={errRange.max || ''}
+                                    onChange={handleMaxErrChange}
+                                />
                             </label>
                     
-                            <button className={style.reload_btn}>
+                            <button className={style.reload_btn} onClick={handleReloadErr}>
                                 <img src={icoReload} alt="" />
                             </button>
                         </div>
