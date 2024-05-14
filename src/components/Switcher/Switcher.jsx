e6aa36682f165b81ec4c444e85132410f6c5cb83
Dash-02 <dasha.semivolos@gmail.com>
2024-05-14 18:07:26 +0300

    switch-work

--- src/components/Switcher/Switcher.jsx
+++ src/components/Switcher/Switcher.jsx
@@ -8,15 +8,15 @@ export const SwitchButton = () => {
   
     const [checked, setChecked] = useState(true)
   
-    const handleChange = nextChecked => {
-        setChecked(nextChecked)
+    const handleChange = () => {
+      setChecked(!checked)
     }
 
   return (
     <>
-        <div htmlFor='switchElement' className={style.switchWrapper}>
-            {/* <img src={moon} alt="" />
-            <img src={sun} alt="" /> */}
+        {/* <div className={style.switchWrapper}>
+            <img src={moon} alt="" />
+            <img src={sun} alt="" />
             <input 
                 type="radio" 
                 id="switchElement"
@@ -24,70 +24,23 @@ export const SwitchButton = () => {
                 checked={checked}
                 onChange={handleChange}
             />
-        </div>
-      {/* <label htmlFor='small-radius-switch'>
-        <Switch
-          checked={checked}
-          onChange={handleChange}
-          handleDiameter={19}
-          offColor='#212121'
-          onColor='#DEDEDE'
-          offHandleColor='#DEDEDE'
-          onHandleColor='#212121'
-          height={24}
-          width={46}
-          borderRadius={20}
-          uncheckedIcon={
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100%',
-                fontSize: 15,
-                color: 'orange',
-                paddingRight: 2,
-              }}
-            ></div>
-          }
-          checkedIcon={''}
-          uncheckedHandleIcon={
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100%',
-                fontSize: 20,
-              }}
-            >
-              <img
-                width={20}
-                height={20}
-                style={{ borderRadius: 20 }}
-                src={moon}
-                alt=''
-                srcset=''
-              />
-            </div>
-          }
-          checkedHandleIcon={
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                alignContent: 'center',
-                height: '100%',
-              }}
-            >
-              <img width={19} height={16} src={sun} alt='' srcset='' />
-            </div>
-          }
-          className='react-switch'
-          id='small-radius-switch'
-        />
-      </label> */}
+        </div> */}
+        <label htmlFor='switch' className={style.switchWrapper}>
+          <input 
+            type='checkbox' 
+            id='switch'
+            className={style.round}
+            checked={checked}
+            onChange={handleChange}
+          />
+          {/* <label htmlFor="switch" className={style.switchLabel}></label> */}
+          {/* <input 
+            type="radio" 
+            className={style.sun}
+            checked={checked}
+            onChange={handleChange}
+          /> */}
+        </label>
     </>
   )
 }
